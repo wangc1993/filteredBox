e755a89c98a3206e508523fef0cd04977d12c82a
wangc1993 <30887676+wangc1993@users.noreply.github.com>
2018-07-23 10:24:41 +0800

    second commit

--- filteredBox.vue
+++ filteredBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="filteredBox">
+  <div class="filteredBox" v-show="tagArr.length === 0 ? false : true">
     <span v-for="(tag, index) in tagArr" :key="index" class="filterTag" :class="tag.name.length > 9 ? 'filterTag-2' : ''">
       {{tag.name}}
       <i @click="deleteTag(index)" class="el-icon-close filterTag-icon"></i>
@@ -30,43 +30,38 @@
     width: 100%;
     min-width: 1366px;
     background-color: #f8f9f9;
-    display: -webkit-box;
-    margin-top: 10px;
-    .filterTag-2 {
-      width: calc((100% - 128px)/4 + 16px) !important;
-    }
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding: 4px;
+    text-align: center;
     .filterTag {
-      display: inline-block;
       position: relative;
-      width: calc((100% - 128px)/8);
-      background-color: #ddd;
-      padding: 0 10px;
-      height: 32px;
-      line-height: 30px;
-      text-align: center;
-      font-size: 12px;
+      width: calc((100% - 72px)/8);
+      font-size: 14px;
       color: #333;
-      border-radius: 4px;
-      box-sizing: border-box;
-      border: 1px solid #ddd;
-      white-space: nowrap;
-      margin: 5px 8px;
-      .filterTag-icon {
-        border-radius: 50%;
-        text-align: center;
-        position: absolute;
-        cursor: pointer;
-        font-size: 12px;
-        height: 16px;
-        width: 16px;
-        line-height: 16px;
-        right: 5px;
-        top: 7px;
-        color: #999;
-        &:hover {
-          background-color: #409EFF;
-          color: #fff;
-        }
+      background-color: #ddd;
+      height: 32px;
+      line-height: 32px;
+      margin: 4px;
+    }
+    .filterTag-2 {
+      width: calc((100% - 72px)/4 + 8px);
+    }
+    .filterTag-icon {
+      border-radius: 50%;
+      position: absolute;
+      cursor: pointer;
+      font-size: 12px;
+      height: 16px;
+      width: 16px;
+      line-height: 16px;
+      right: 4px;
+      top: 8px;
+      color: #999;
+      &:hover {
+        background-color: #409EFF;
+        color: #fff;
       }
     }
   }
